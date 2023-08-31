1559d011e7f4f109377145503c6b7b93e5ecf590
Alan Braulio <alan-braulio@hotmail.com>
2023-08-31 00:21:33 -0300

    initializing project

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import fastify from "fastify";
+import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_KEY!);
+
+type SendGrid = {
+  to: string;
+  from: string;
+  subject: string;
+  text: string;
+  html: string;
+};
+
+const app = fastify();
+
+app.post("/send-email", async (request, response) => {
+    const { to, from, subject, text, html } = request.body as SendGrid;
+  
+    const msg = { to, from, subject, text, html };
+  
+    try {
+      const res = await sgMail.send(msg);
+      const status = res[0].statusCode;
+      console.log("Email sent successfully. Status code:", status);
+      response.status(200).send('Email enviado com sucesso');
+    } catch (error) {
+      console.error("Error sending email:", error);
+      response.status(500).send('Erro ao enviar o email');
+    }
+  });
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log("Servidor funcionando");
+  });
